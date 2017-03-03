8d9f14c3679f7567673645bfcb637cb36e6386e9
thiago mendes <thiago mendes>
2017-03-03 18:47:44 -0300

    atualização heroku db

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'us-cdbr-iron-east-04.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'teste'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_1059abc32e65274'),
+            'username' => env('DB_USERNAME', 'bbad3e56bc8439'),
+            'password' => env('DB_PASSWORD', 'b0453466'),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
